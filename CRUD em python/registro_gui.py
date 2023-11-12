e67543e9699c6bf0f2d19a0025753d607b2e0e3f
Rayany Costa <150172842+IRayany@users.noreply.github.com>
2023-11-12 03:27:53 -0300

    registro_gui.py

--- CRUD em python/registro_gui.py
+++ CRUD em python/registro_gui.py
@@ -12,17 +12,21 @@ class RegistroGUI:
         frame = tk.Frame(self.parent)
         frame.pack(expand=True, fill='both')
 
-        tree = ttk.Treeview(frame, columns=("Raça", "Nome", "Peso", "Data da Consulta", "Próxima Consulta"), height=10, show="headings")  
+        tree = ttk.Treeview(frame, columns=("Raça", "Nome", "Peso", "Data da Consulta", "Próxima Consulta", "Valor do Medicamento", "Valor Total"), height=10, show="headings")  
         tree.column("#1", width=90, anchor="center")  
         tree.column("#2", width=90, anchor="center")
         tree.column("#3", width=90, anchor="center")
         tree.column("#4", width=90, anchor="center")
         tree.column("#5", width=90, anchor="center")
+        tree.column("#6", width=90, anchor="center")
+        tree.column("#7", width=90, anchor="center")
         tree.heading('#1', text='Raça')
         tree.heading('#2', text='Nome')
         tree.heading('#3', text='Peso')
         tree.heading('#4', text='Data da Consulta')
         tree.heading('#5', text='Próxima Consulta')
+        tree.heading('#6', text='Valor do Medicamento')
+        tree.heading('#7', text='Valor Total')
 
         vsb = ttk.Scrollbar(frame, orient="vertical", command=tree.yview)
         vsb.pack(side='right', fill='y')
@@ -39,7 +43,7 @@ class RegistroGUI:
         tree.pack(fill='both', expand=True)
 
         
-        for col in ("Raça", "Nome", "Peso", "Data da Consulta", "Próxima Consulta"):
+        for col in ("Raça", "Nome", "Peso", "Data da Consulta", "Próxima Consulta", "Valor do Medicamento", "Valor Total"):
             tree.heading(col, command=lambda _col=col: self.sort_treeview(tree, _col, False))
             tree.column(col, width=30)
 
